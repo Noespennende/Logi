5c04b66cfc7a41c363681fe9ccdc94b8cd7720fb
Mathilde <111729553+mathildeolesen@users.noreply.github.com>
2025-04-12 18:46:37 +0200

    Feat: PP_Logi_ThermalCamera - second "box" finished - Add noise
    Nodes related to "Add noise" is created.
    
    ❗The creation of ViewSize node also needs the workaround of a MaterialFunction uasset file with C++ in 5.3.2.
    * The workaround is the usage of the saved uasset file MF_Logi_ViewSize, which is a MaterialFunction node that hold the ViewSize node. We then connect this MaterialFunction node to where the ViewSize node is supposed to go.

--- Source/Logi/Private/PP_Logi_ThermalCamera.cpp
+++ Source/Logi/Private/PP_Logi_ThermalCamera.cpp
@@ -23,12 +23,20 @@
 #include "Materials/Material.h"
 #include "Materials/MaterialExpressionSceneTexture.h"
 #include "CoreUObject.h"
+#include "Materials/MaterialExpressionAdd.h"
+#include "Materials/MaterialExpressionAppendVector.h"
+#include "Materials/MaterialExpressionFloor.h"
+#include "Materials/MaterialExpressionMultiply.h"
+#include "Materials/MaterialExpressionTextureCoordinate.h"
+#include "Materials/MaterialExpressionTime.h"
+#include "Materials/MaterialExpressionVectorNoise.h"
+#include "Materials/MaterialExpressionViewSize.h"
 
 
 /// 
 
 
-UMaterialExpressionCollectionParameter* CreateCollectionParameterNode(UMaterial* Material, FVector2D EditorPos)
+UMaterialExpressionCollectionParameter* CreateThermalSettingsCPNode(UMaterial* Material, FVector2D EditorPos, const FName& ParameterName)
 {
     UE_LOG(LogTemp, Warning, TEXT("Starting to create Collection Parameter Node"));
 
@@ -47,7 +55,7 @@ UMaterialExpressionCollectionParameter* CreateCollectionParameterNode(UMaterial*
     UMaterialExpressionCollectionParameter* CollectionParamNode = NewObject<UMaterialExpressionCollectionParameter>(Material);
     
     CollectionParamNode->Collection = MPC_ThermalSettings;
-    CollectionParamNode->ParameterName = TEXT("ThermalCameraToggle");
+    CollectionParamNode->ParameterName = ParameterName;
 
     // Set the nodes psoition in the editor
     CollectionParamNode->MaterialExpressionEditorX = EditorPos.X;
@@ -59,12 +67,19 @@ UMaterialExpressionCollectionParameter* CreateCollectionParameterNode(UMaterial*
     // (This is needed as the Guid does not automatically transfer with MPC_ThermalSettings when creating the node)
     for (const FCollectionScalarParameter& Param : MPC_ThermalSettings->ScalarParameters)
     {
+        //Log
+        UE_LOG(LogTemp, Warning, TEXT("Parameter Name: %s"), *Param.ParameterName.ToString());
         // Iterates every parameter in ScalarParameters and checks for the wanted parameter name
         if (Param.ParameterName == CollectionParamNode->ParameterName)
         {
+            //Log
+            UE_LOG(LogTemp, Warning, TEXT("Found parameter: %s"), *Param.ParameterName.ToString());
             CollectionParamNode->ParameterId = Param.Id;
             break;
         }
+
+        //Log
+        UE_LOG(LogTemp, Warning, TEXT("Did not find: %s"), *Param.ParameterName.ToString());
     }
     
     // Make sure the CollectionParamNode now has a valid collection assigned
@@ -114,13 +129,14 @@ UMaterialExpressionMaterialFunctionCall* CreateSceneTexture(UMaterial* Material,
 }
 
 
-UMaterialExpressionComment* CreateCommentNode(UMaterial* Material, FVector2D EditorPos, FVector2D Size, const FString& CommentText)
+UMaterialExpressionComment* CreateCommentNode(UMaterial* Material, FVector2D EditorPos, FVector2D Size, const FString& CommentText, FLinearColor BoxColor = FLinearColor::White)
 {
     UMaterialExpressionComment* Comment = NewObject<UMaterialExpressionComment>(Material);
 
     // Posititon in editor
     Comment->MaterialExpressionEditorX = EditorPos.X;
     Comment->MaterialExpressionEditorY = EditorPos.Y;
+    
 
     // Size of the comment box
     Comment->SizeX = Size.X;
@@ -129,7 +145,130 @@ UMaterialExpressionComment* CreateCommentNode(UMaterial* Material, FVector2D Edi
     // Comment text
     Comment->Text = CommentText;
 
+    // Color of the comment box
+    Comment->CommentColor = BoxColor; // Grey color
+    // Comment color #FFF976FF
+    // Comment->CommentColor = FLinearColor(1.0f, 1.0f, 1.0f, 1.0f); // White color
+
     return Comment;
+}
+
+UMaterialExpressionAdd* CreateAddNode(UMaterial* Material, FVector2D EditorPos)
+{
+    UMaterialExpressionAdd* AddNode = NewObject<UMaterialExpressionAdd>(Material);
+
+    // Posititon in editor
+    AddNode->MaterialExpressionEditorX = EditorPos.X;
+    AddNode->MaterialExpressionEditorY = EditorPos.Y;
+    
+    return AddNode;
+}
+
+UMaterialExpressionComponentMask* CreateMaskNode(UMaterial* Material, FVector2D EditorPos, bool R, bool G, bool B)
+{
+
+    UMaterialExpressionComponentMask* MaskNode = NewObject<UMaterialExpressionComponentMask>(Material);
+    MaskNode->MaterialExpressionEditorX = EditorPos.X;
+    MaskNode->MaterialExpressionEditorY = EditorPos.Y;
+
+    MaskNode->R = R; // Activate R-channel
+    MaskNode->G = G; // Activate G-channel
+    MaskNode->B = B; // Activate B-channel
+
+    return MaskNode;
+}
+
+UMaterialExpressionVectorNoise* CreateVectorNoiseNode(UMaterial* Material, FVector2D EditorPos)
+{
+
+    UMaterialExpressionVectorNoise* VectorNoiseNode = NewObject<UMaterialExpressionVectorNoise>(Material);
+    VectorNoiseNode->MaterialExpressionEditorX = EditorPos.X;
+    VectorNoiseNode->MaterialExpressionEditorY = EditorPos.Y;
+
+    return VectorNoiseNode;
+}
+
+UMaterialExpressionAppendVector* CreateAppendVectorNode(UMaterial* Material, FVector2D EditorPos)
+{
+
+    UMaterialExpressionAppendVector* AppendVectorNode = NewObject<UMaterialExpressionAppendVector>(Material);
+    AppendVectorNode->MaterialExpressionEditorX = EditorPos.X;
+    AppendVectorNode->MaterialExpressionEditorY = EditorPos.Y;
+
+    return AppendVectorNode;
+}
+
+UMaterialExpressionMultiply* CreateMultiplyNode(UMaterial* Material, FVector2D EditorPos)
+{
+
+    UMaterialExpressionMultiply* MultiplyNode = NewObject<UMaterialExpressionMultiply>(Material);
+    MultiplyNode->MaterialExpressionEditorX = EditorPos.X;
+    MultiplyNode->MaterialExpressionEditorY = EditorPos.Y;
+
+    return MultiplyNode;
+}
+
+UMaterialExpressionTime* CreateTimeNode(UMaterial* Material, FVector2D EditorPos)
+{
+
+    UMaterialExpressionTime* TimeNode = NewObject<UMaterialExpressionTime>(Material);
+    TimeNode->MaterialExpressionEditorX = EditorPos.X;
+    TimeNode->MaterialExpressionEditorY = EditorPos.Y;
+
+    return TimeNode;
+}
+
+UMaterialExpressionConstant* CreateConstantNode(UMaterial* Material, FVector2D EditorPos, float Value)
+{
+    UMaterialExpressionConstant* ConstantNode = NewObject<UMaterialExpressionConstant>(Material);
+    ConstantNode->MaterialExpressionEditorX = EditorPos.X;
+    ConstantNode->MaterialExpressionEditorY = EditorPos.Y;
+    ConstantNode->R = Value; 
+
+    return ConstantNode;
+}
+
+UMaterialExpressionFloor* CreateFloorNode(UMaterial* Material, FVector2D EditorPos)
+{
+    UMaterialExpressionFloor* FloorNode = NewObject<UMaterialExpressionFloor>(Material);
+    FloorNode->MaterialExpressionEditorX = EditorPos.X;
+    FloorNode->MaterialExpressionEditorY = EditorPos.Y;
+
+    return FloorNode;
+}
+
+UMaterialExpressionTextureCoordinate* CreateTextureCoordinateNode(UMaterial* Material, FVector2D EditorPos)
+{
+    UMaterialExpressionTextureCoordinate* TextureCoordinateNode = NewObject<UMaterialExpressionTextureCoordinate>(Material);
+    TextureCoordinateNode->MaterialExpressionEditorX = EditorPos.X;
+    TextureCoordinateNode->MaterialExpressionEditorY = EditorPos.Y;
+
+    return TextureCoordinateNode;
+}
+
+UMaterialExpressionMaterialFunctionCall* CreateViewSize(UMaterial* Material, FVector2D EditorPos)
+{
+
+    // 1) Load the MF asset with SceneTexturePP functionality inside
+    FString FilePath = TEXT("/Logi/MF_Logi_ViewSize.MF_Logi_ViewSize");
+    UMaterialFunction* MaterialFunction = LoadObject<UMaterialFunction>(nullptr, *FilePath);
+
+    if (!MaterialFunction)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("MaterialFunction failed to load!"));
+        return nullptr;
+    }
+    
+
+    // 2) Create MaterialExpressionFunctionCall node, and assign the MF asset to it
+    UMaterialExpressionMaterialFunctionCall* MFViewSizeNode = NewObject<UMaterialExpressionMaterialFunctionCall >(Material);
+    MFViewSizeNode->MaterialFunction = MaterialFunction;  // Set the Function to the MF-asset from 1)
+
+    // Nodes posititon in editor
+    MFViewSizeNode->MaterialExpressionEditorX = EditorPos.X;
+    MFViewSizeNode->MaterialExpressionEditorY = EditorPos.Y;
+
+    return MFViewSizeNode;
 }
 
 
@@ -169,16 +308,19 @@ void FPP_ThermalCamera::CreateThermalCamera(bool& mfNodecreated, FString& status
     
     // The list of nodes in the MaterialFunction - We will be adding the nodes to this list
     TArray<TObjectPtr<UMaterialExpression>>& Expressions = Material->GetExpressionCollection().Expressions;
-    
+
+
+    /* 1 - White area */
+
     // LERP-node
     FVector2D WhiteLerpNodePos(-300, 200);
-    UMaterialExpressionLinearInterpolate* NodeLerp = CreateLerpNode(Material, WhiteLerpNodePos);
-    Expressions.Add(NodeLerp);
+    UMaterialExpressionLinearInterpolate* WhiteLerpNode = CreateLerpNode(Material, WhiteLerpNodePos);
+    Expressions.Add(WhiteLerpNode);
     
-    // CollectionParameter-node
-    FVector2D CollectionParamNodePos(-550, 300);
-    UMaterialExpressionCollectionParameter* CollectionParamNode = CreateCollectionParameterNode(Material, CollectionParamNodePos);
-    Expressions.Add(CollectionParamNode);
+    // ThermalSettingsCameraToggle-node
+    FVector2D ThermalSettingsCameraToggleNodePos(-550, 300);
+    UMaterialExpressionCollectionParameter* ThermalSettingsCameraToggleNode = CreateThermalSettingsCPNode(Material, ThermalSettingsCameraToggleNodePos, TEXT("ThermalCameraToggle"));
+    Expressions.Add(ThermalSettingsCameraToggleNode);
     
     // MF with SceneTexture-PostProcessInput0 inside -node
     FVector2D MFSceneTextureNodePos(-600, 150);
@@ -190,25 +332,194 @@ void FPP_ThermalCamera::CreateThermalCamera(bool& mfNodecreated, FString& status
     FVector2D WhiteCommentSize(500, 450);
     FString WhiteCommentText = TEXT("Is thermal camera on?");
     UMaterialExpressionComment* Comment = CreateCommentNode(Material,WhiteCommentPos, WhiteCommentSize, WhiteCommentText);
-    
     Expressions.Add(Comment);
 
-
-    
-    /* LINKING */
+    /* Linking */
 
     // Connect Lerp node to EmissiveColor
-    Material->GetEditorOnlyData()->EmissiveColor.Connect(0, NodeLerp);
-    
-    
+    Material->GetEditorOnlyData()->EmissiveColor.Connect(0, WhiteLerpNode);
+
     // Connect MFSceneTexture node to B input of Lerp node
     MFSceneTextureNode->UpdateFromFunctionResource();
-    NodeLerp->A.Connect(0, MFSceneTextureNode);
+    WhiteLerpNode->A.Connect(0, MFSceneTextureNode);
 
-    NodeLerp->Alpha.Connect(0, CollectionParamNode);
+    // Connect (CollectionParam) MPC_ThermalSettings node  to A input of Lerp node
+    WhiteLerpNode->Alpha.Connect(0, ThermalSettingsCameraToggleNode);
+
+
+    /* 2 - Yellow area */
+    
+    // LERP-node
+    FVector2D YelloLerpNodePos(-1000, 210);
+    UMaterialExpressionLinearInterpolate* YellowLerpNode = CreateLerpNode(Material, YelloLerpNodePos);
+    Expressions.Add(YellowLerpNode);
+
+    // (Connects Yellow area to White area)
+    WhiteLerpNode->B.Connect(0, YellowLerpNode);
+
+
+    // ThermalSettingsNoiseAmount-node
+    FVector2D ThermalSettingsNoiseAmountPos(-1255, 400);
+    UMaterialExpressionCollectionParameter* ThermalSettingsNoiseAmountNode = CreateThermalSettingsCPNode(Material, ThermalSettingsNoiseAmountPos, TEXT("NoiseAmount"));
+    Expressions.Add(ThermalSettingsNoiseAmountNode);
+
+    YellowLerpNode->Alpha.Connect(0, ThermalSettingsNoiseAmountNode);
+
+    // Add-node
+    FVector2D AddNodePos(-1255, 280);
+    UMaterialExpressionAdd* AddNode = CreateAddNode(Material, AddNodePos);
+    Expressions.Add(AddNode);
+
+    YellowLerpNode->B.Connect(0, AddNode);
+
+
+    /** Add noise to image area **/
+    
+    // Mask-node
+    FVector2D NoiseMaskPos(-1550, 450);
+    UMaterialExpressionComponentMask* NoiseMaskNode = CreateMaskNode(Material, NoiseMaskPos, true, false, false);
+    Expressions.Add(NoiseMaskNode);
+    
+    AddNode->B.Connect(0, NoiseMaskNode);
+
+    // VectorNoise-node
+    FVector2D VectorNoisePos(-1750, 450);
+    UMaterialExpressionVectorNoise* VectorNoiseNode = CreateVectorNoiseNode(Material, VectorNoisePos);
+    Expressions.Add(VectorNoiseNode);
+
+    NoiseMaskNode->Input.Connect(0, VectorNoiseNode);
+
+
+    /*** Convert vector 2 to vector 3 ***/
+    
+    // AppendVector-node
+    FVector2D AppendVectorPos(-2000, 460);
+    UMaterialExpressionAppendVector* AppendVectorNode = CreateAppendVectorNode(Material, AppendVectorPos);
+    Expressions.Add(AppendVectorNode);
+
+    // Connect AppendVector to VectorNoise
+    VectorNoiseNode->Position.Connect(0, AppendVectorNode);
+
+
+    // Multiply-node
+    FVector2D ConvertMultiplyNodePos(-2200, 600);
+    UMaterialExpressionMultiply* ConvertMultiplyNode = CreateMultiplyNode(Material, ConvertMultiplyNodePos);
+    Expressions.Add(ConvertMultiplyNode);
+
+
+    AppendVectorNode->B.Connect(0, ConvertMultiplyNode);
+
+    // Time-node
+    FVector2D TimeNodePos(-2350, 585);
+    UMaterialExpressionTime* TimeNode = CreateTimeNode(Material, TimeNodePos);
+    Expressions.Add(TimeNode);
+
+    ConvertMultiplyNode->A.Connect(0, TimeNode);
+
+
+    // Constant-node
+    FVector2D ConstantNodePos(-2380, 700);
+    UMaterialExpressionConstant* ConstantNode = CreateConstantNode(Material, ConstantNodePos, 60.0f);
+    Expressions.Add(ConstantNode);
+
+    ConvertMultiplyNode->B.Connect(0, ConstantNode);
+
+    
+    // Yellow inside comment node - Convert vector 2 to vector 3
+    FVector2D CommentYellowConvertPos(-2420, 400);
+    FVector2D CommentYellowConvertSize(600, 500);
+    FString CommentYellowConvertText = TEXT("Convert vector 2 to vector 3");
+    UMaterialExpressionComment* CommentYellowConvert = CreateCommentNode(Material, CommentYellowConvertPos, CommentYellowConvertSize, CommentYellowConvertText);
+    Expressions.Add(CommentYellowConvert);
+
+
+    /*** Deside size of noise pixels ***/
+
+    // Multiply-node
+    FVector2D DesideMultiplyNodePos(-2600, 460);
+    UMaterialExpressionMultiply* DesideMultiplyNode = CreateMultiplyNode(Material, DesideMultiplyNodePos);
+    Expressions.Add(DesideMultiplyNode);
+
+    AppendVectorNode->A.Connect(0, DesideMultiplyNode);
+    
+    // Mask-node
+    FVector2D DesideMaskNodePos(-2750, 600);
+    UMaterialExpressionComponentMask* DesideMaskNode = CreateMaskNode(Material, DesideMaskNodePos, true, true, false);
+    Expressions.Add(DesideMaskNode);
+
+    DesideMultiplyNode->B.Connect(0, DesideMaskNode);
+
+    
+    // MPC_ThermalSettings node
+    FVector2D ThermalSettingsNoiseSizePos(-3000, 640);
+    UMaterialExpressionCollectionParameter* ThermalSettingsNoiseSizeNode = CreateThermalSettingsCPNode(
+        Material, ThermalSettingsNoiseSizePos, TEXT("NoiseSize"));
+    Expressions.Add(ThermalSettingsNoiseSizeNode);
+
+    DesideMaskNode->Input.Connect(0, ThermalSettingsNoiseSizeNode);
+    
+    // Yellow inside comment node 2 - Deside size of noise pixels
+    FVector2D CommentYellowDesidePos(-3030, 400);
+    FVector2D CommentYellowDesideSize(600, 500);
+    FString CommentYellowDesideText = TEXT("Deside size of noise pixels");
+    UMaterialExpressionComment* CommentYellowDeside = CreateCommentNode(Material, CommentYellowDesidePos, CommentYellowDesideSize, CommentYellowDesideText);
+    Expressions.Add(CommentYellowDeside);
+
+
+    /*** Get screen coordinates ***/
+
+    // Floor-node
+    FVector2D CoordinatesFloorNodePos(-3200, 460);
+    UMaterialExpressionFloor* CoordinatesFloorNode = CreateFloorNode(Material, CoordinatesFloorNodePos);
+    Expressions.Add(CoordinatesFloorNode);
+    
+    DesideMultiplyNode->A.Connect(0, CoordinatesFloorNode);
     
 
+    // Multiply-node
+    FVector2D CoordinatesMultiplyNodePos(-3400, 460);
+    UMaterialExpressionMultiply* CoordinatesMultiplyNode = CreateMultiplyNode(Material, CoordinatesMultiplyNodePos);
+    Expressions.Add(CoordinatesMultiplyNode);
 
+    CoordinatesFloorNode->Input.Connect(0, CoordinatesMultiplyNode);
+
+    
+    // TextureCoordinate-node
+    FVector2D CoordinatesTextureCoordinateNodePos(-3620, 460);
+    UMaterialExpressionTextureCoordinate* CoordinatesTextureCoordinateNode = CreateTextureCoordinateNode(Material, CoordinatesTextureCoordinateNodePos);
+    Expressions.Add(CoordinatesTextureCoordinateNode);
+
+    CoordinatesMultiplyNode->A.Connect(0, CoordinatesTextureCoordinateNode);
+
+    
+    // MF with ViewSize inside -node
+    FVector2D CoordinatesViewSizeNodePos(-3620, 600);
+    UMaterialExpressionMaterialFunctionCall* CoordinatesViewSizeNode = CreateViewSize(Material, CoordinatesViewSizeNodePos);
+    Expressions.Add(CoordinatesViewSizeNode);
+
+    CoordinatesViewSizeNode->UpdateFromFunctionResource();
+    CoordinatesMultiplyNode->B.Connect(0, CoordinatesViewSizeNode);
+    
+
+    // Yellow inside comment node 2 - Deside size of noise pixels
+    FVector2D CommentYellowCoordinatesPos(-3650, 400);
+    FVector2D CommentYellowCoordinatesSize(600, 500);
+    FString CommentYellowCoordinatesText = TEXT("Get screen coordinates");
+    UMaterialExpressionComment* CommentYellowCoordinates = CreateCommentNode(Material, CommentYellowCoordinatesPos, CommentYellowCoordinatesSize, CommentYellowCoordinatesText);
+    Expressions.Add(CommentYellowCoordinates);
+
+
+    // 
+    // Yellow comment box (2) - Add noise
+    FVector2D YellowCommentPos(-3700, 75);
+    FVector2D YellowCommentSize(2900, 900);
+    FString YellowCommentText = TEXT("Add noise");
+    FColor YellowCommentColor =FColor::FromHex(TEXT("FFF976FF"));
+    UMaterialExpressionComment* YellowComment = CreateCommentNode(Material, YellowCommentPos, YellowCommentSize, YellowCommentText, YellowCommentColor);
+    
+    Expressions.Add(YellowComment);
+    
+    
     /* Finish */
 
     // Mark dirty ("There's been changes in the package/file!")
